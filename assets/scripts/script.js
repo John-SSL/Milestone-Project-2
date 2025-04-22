8e5df375c2191f43e58bbb031407433b286b8ff9
John-SSL <jsscadden@gmail.com>
2025-04-22 16:32:33 +0000

    Add JavaScript to shuffle symbols when choice is correct

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,37 +1,52 @@
 
-const symbols = document.querySelectorAll(".symbol");
-const sources = [];
-const turns = [];
+// // Shuffles cards and choses random symbol on page load
+window.addEventListener("load", function () {
+    shuffleTurns();
+    pickSymbol();
+});
 
-// stores the src of each element into an array
+const sources = [];
+let turns = [];
+let symbols = document.querySelectorAll(".symbol");
+let correctSymbol = "";
+
+// // stores the src of each element into an array
 for (let symbol of symbols) {
     sources.push(symbol.src);
 }
 
-// Stores unique number between 0 and 17 into turns array
-while (turns.length < 18) {
-    let randomNumber = Math.floor(Math.random() * 18);
-
-    if (turns.includes(randomNumber) === false){
-        turns.push(randomNumber);
+// Stores unique sequences of numbers between 0 and 17 into turns array
+function shuffleTurns () {
+    turns = [];
+    while (turns.length < 18) {
+        let randomNumber = Math.floor(Math.random() * 18);
+    
+        if (turns.includes(randomNumber) === false){
+            turns.push(randomNumber);
+        };
+    };
+    // Shuffles symbols in cards
+    console.log(turns);
+    for (let i = 0; i < 18; i++) {
+    symbols[i].src = sources[turns[i]];
     };
 }
 
-// Shuffles symbols in cards
-for (let i = 0; i < 18; i++){
-    symbols[i].src = sources[turns[i]];
-}
 
 // Pick a random symbol to display on both cards
-const card1Symbols = document.getElementsByClassName("card1-symbol");
-const card2Symbols = document.getElementsByClassName("card2-symbol");
-const randomSymbol = card1Symbols[Math.floor(Math.random() * 9)];
-card2Symbols[Math.floor(Math.random() * 9)].src = randomSymbol.src;
+function pickSymbol () {
+    const card1Symbols = document.getElementsByClassName("card1-symbol");
+    const card2Symbols = document.getElementsByClassName("card2-symbol");
+    const randomSymbol = card1Symbols[Math.floor(Math.random() * 9)];
+    card2Symbols[Math.floor(Math.random() * 9)].src = randomSymbol.src;
+    
+    correctSymbol = randomSymbol;
+    console.log(randomSymbol.src);
+}
 
-console.log(randomSymbol.src);
 
 // Add event listeners to all symbols
-for (let symbol of card2Symbols) {
+for (let symbol of document.getElementsByClassName("card2-symbol")) {
     symbol.addEventListener("click", responseCheck);
 }
 
@@ -40,8 +55,9 @@ for (let symbol of card2Symbols) {
  */
 function responseCheck (e) {
     console.log(e.srcElement.src);
-    if (e.srcElement.src === randomSymbol.src){
-        prompt("Working!");
+    if (e.srcElement.src === correctSymbol.src){
+        shuffleTurns();
+        pickSymbol();
     };
 }
 
